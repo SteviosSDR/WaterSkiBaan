3c0ae984eb64450386145b0141ea8d559c13171e
SteviosSDR <46400761+SteviosSDR@users.noreply.github.com>
2019-10-16 21:23:01 +0200

    opdracht 12 deels
    -mid term commit

--- waterskibaan/waterskibaan/Game.cs
+++ waterskibaan/waterskibaan/Game.cs
@@ -10,15 +10,25 @@ namespace waterskibaan
     public class Game
     {
         Waterskibaan waterskibaan = new Waterskibaan();
+        WachtrijInstructie instructierij = new WachtrijInstructie();
+        InstructieGroep instructieGroep = new InstructieGroep();
+        WachtrijStarten startRij = new WachtrijStarten();
 
-        public delegate void NieuweBezoekerHandler(NieuweBezoekerHandler args);
+        public delegate void NieuweBezoekerHandler(NieuweBezoekerArgs args);
+        public delegate void InstructieAfgelopenHandler(InstructieAfgelopenArgs args);
 
+        public event InstructieAfgelopenHandler InstructieAfgelopen;
         public event NieuweBezoekerHandler NieuweBezoeker;
+
+        //timers
+        private static System.Timers.Timer gametimer;
+
+        private int sec;
+
         public void initialize()
         {
-            WachtrijInstructie instructierij = new WachtrijInstructie();
-            InstructieGroep instructieGroep = new InstructieGroep();
-            WachtrijStarten startRij = new WachtrijStarten();
+            NieuweBezoeker += NewBezoeker;
+            InstructieAfgelopen += VInstructieAfgelopen;
             waterskibaan.waterskibaan();
 
             GameLoop();
@@ -26,27 +36,76 @@ namespace waterskibaan
 
         public void GameLoop()
         {
-            Timer gametimer = new Timer();
-            gametimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            gametimer.Interval = 1000;
+            gametimer = new Timer(1000);
+            sec++;
+            if(sec%2 == 0)
+            {
+                gametimer.Elapsed += NewSporter;
+            }
+
+            else if (sec%20 == 0)
+            {
+                gametimer.Elapsed += NewInstructieAfgelopen;
+            }
+
             gametimer.AutoReset = true;
             gametimer.Enabled = true;
+
             Console.ReadLine();
             gametimer.Stop();
             gametimer.Dispose();
         }
 
-        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        private void NewInstructieAfgelopen(Object source, ElapsedEventArgs e)
         {
+            InstructieAfgelopen?.Invoke(new InstructieAfgelopenArgs(instructierij.SportersVerlatenRij(5)));
+        }
+
+        private void NewSporter(Object source, ElapsedEventArgs e)
+        {
+            //opdracht 11
+            /* 
             Sporter henk = new Sporter(MoveCollection.GetWillekeurigeMoves(5));
             waterskibaan.VerplaatsKabel();
             waterskibaan.SporterStart(henk);
             Console.WriteLine(waterskibaan.ToString());
+            */
+
+            NieuweBezoeker?.Invoke(new NieuweBezoekerArgs(new Sporter(MoveCollection.GetWillekeurigeMoves(5))));
         }
 
-        public class NieuweBezoekerArgs
+        private void NewBezoeker(NieuweBezoekerArgs args)
         {
+            //Console.WriteLine("Sporter toegevoegd aan wachtrij");
+            instructierij.SporterneemPlaatsInRij(args.Sporter);
+        }
 
+        private void VInstructieAfgelopen(InstructieAfgelopenArgs args)
+        {
+            foreach(Sporter sporter in args.sporterLijst)
+            {
+                instructieGroep.SporterneemPlaatsInRij(sporter);
+            }
+        }
+
+        public class NieuweBezoekerArgs : EventArgs
+        {
+            public Sporter Sporter { get;}
+
+            public NieuweBezoekerArgs(Sporter sporter)
+            {
+                Sporter = sporter;
+            }
+        }
+
+        public class InstructieAfgelopenArgs : EventArgs
+        {
+            public List<Sporter> sporterLijst { get; }
+
+            public InstructieAfgelopenArgs(List<Sporter> sporterlist)
+            {
+                sporterLijst = sporterlist;
+            }
         }
     }
 }
